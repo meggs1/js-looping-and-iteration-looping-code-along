691236035fc74d566787ed4245c96f68bb6f2676
megan <megan.schertz@gmail.com>
2021-04-23 19:37:16 -0500

    commit tree 7f81742f47ff676b17b8fed9ad11d1c06d60fc71

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names) {
+    let cardsArray = [];
+    for (let i = 0; i < names.length; i++) {
+        cardsArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return cardsArray
+}
+
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--);
+    }
+
+}
